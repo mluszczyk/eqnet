0ab0a75dbb6de76b8632f5d6479495e552af2c5c
Michał Łuszczyk <mluszczyk@users.noreply.github.com>
2018-02-23 11:29:20 +0100

    Command line parameters for bool expression generation.

--- data/synthetic/boolexpressions.py
+++ data/synthetic/boolexpressions.py
@@ -11,20 +11,29 @@ from tqdm import tqdm
 from data.datasetgenerator import save_result_as_gzipped_json
 from data.tree import Node
 
-all_symbols = [chr(i) for i in range(97, 101)]
-
 # Name, (preorder, inorder, postorder), child_properties
-non_terminals = [('And', '&', ('left', 'right')),
+complex_non_terminals = [('And', '&', ('left', 'right')),
                  ('Or', '|', ('left', 'right')),
                  ('Xor', '^', ('left', 'right')),
                  ('Implies', '>>', ('left', 'right')),
                  ('Not', '~', ('child',)),
                  ]
 
-print_instructions = {s[0]: s[1] for s in non_terminals}
+basic_non_terminals = [('And', '&', ('left', 'right')),
+                 ('Or', '|', ('left', 'right')),
+                 ('Not', '~', ('child',)),
+                 ]
+
+print_instructions = {s[0]: s[1] for s in complex_non_terminals}
 
 
-def generate_all_trees(current_tree, max_tree_size=7):
+def all_symbols(symbol_num):
+    limit = ord('a') + symbol_num
+    assert limit <= ord('z') + 1
+    return [chr(i) for i in range(ord('a'), limit)]
+
+
+def generate_all_trees(current_tree, max_tree_size, non_terminals, symbol_num):
     if len(current_tree) > max_tree_size + 1:
         return
 
@@ -35,19 +44,19 @@ def generate_all_trees(current_tree, max_tree_size=7):
         yield current_tree
         return
 
-    for symbol in all_symbols:
+    for symbol in all_symbols(symbol_num):
         tree_copy = deepcopy(current_tree)
         node, property = next((n, p) for n in tree_copy for p in n.properties if len(n[p]) == 0)
         child = Node(symbol, (), parent=node)
         node.set_children_for_property(property, (child,))
-        yield from generate_all_trees(tree_copy)
+        yield from generate_all_trees(tree_copy, max_tree_size, non_terminals, symbol_num)
 
     for non_terminal_name, _, properties in non_terminals:
         tree_copy = deepcopy(current_tree)
         node, property = next((n, p) for n in tree_copy for p in n.properties if len(n[p]) == 0)
         child = Node(non_terminal_name, properties, parent=node)
         node.set_children_for_property(property, (child,))
-        yield from generate_all_trees(tree_copy)
+        yield from generate_all_trees(tree_copy, max_tree_size, non_terminals, symbol_num)
 
 
 def to_token_sequence(node: Node, current_tokens: list) -> list:
@@ -95,9 +104,33 @@ def convert_to_dict(node: Node) -> dict:
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage <outputFilenamePrefix>")
+    def usage():
+        print("Usage <outputFilenamePrefix> [maxTreeSize, default=7] [additionalOperators: true/false, default=true] [symbolNum, default=4]")
         sys.exit(-1)
+    if len(sys.argv) < 2 or len(sys.argv) > 5:
+        usage()
+
+    if len(sys.argv) < 3:
+         max_tree_size = 7
+    else:
+         max_tree_size = int(sys.argv[2])
+
+    if len(sys.argv) < 4:
+         additional_operators = True
+    else:
+         val = sys.argv[3]
+         if val == "true":
+             non_terminals = complex_non_terminals
+         elif val == "false":
+             non_terminals = basic_non_terminals
+         else:
+             usage()
+
+    if len(sys.argv) < 5:
+        symbol_num = 4
+    else:
+        symbol_num = int(sys.argv[4])
+
     synthesized_expressions = defaultdict(lambda: (list(), 0))
     num_times_returned_no_tree = 0
     num_times_returned_duplicate = 0
@@ -112,7 +145,10 @@ if __name__ == "__main__":
 
     max_num_elements = 500  # max num of expressions per semantically equivalent set to sample (reservoir sampling)
 
-    tree_generator = generate_all_trees(Node('Start', ('child',)))
+    tree_generator = generate_all_trees(Node('Start', ('child',)),
+                                        max_tree_size=max_tree_size,
+                                        non_terminals=non_terminals,
+                                        symbol_num=symbol_num)
     for i, tree in tqdm(enumerate(tree_generator)):
         if i % 5000 == 4999:
             print_stats()
